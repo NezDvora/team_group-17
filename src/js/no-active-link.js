84aa1332763aa43b99a231a39ab7d4b554f5427b
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-03-18 21:54:58 +0200

    add js no-active-link

---
+++ src/js/no-active-link.js
@@ -0,0 +1,5 @@
+const link = document.getElementById('my-link');
+link.addEventListener('click', function (event) {
+  event.preventDefault(); // Предотвращаем обновление страницы
+  // Другой код, который вы хотите выполнить при клике на ссылку
+});
