8d7eaa48baa6d1438c0f3e4cb0be05eae608d45b
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-03-20 19:14:05 +0200

    fix main js

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import 'https://code.jquery.com/jquery-3.6.4.min.js';
+import 'https://code.jquery.com/jquery-migrate-1.2.1.min.js';
+import 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+import './slider';
+import './slider-reviews.js';
+import './modal.js';
+import './mobile-menu.js';
+import './no-active-link.js';
